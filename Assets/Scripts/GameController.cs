c46feb7d07f45c8a7f3a0478bd99b0f25b833d28
piusanggoro <piusanggoro@gmail.com>
2016-12-20 10:42:23 +0700

    for Android

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	//private GUIStyle guiStyle = new GUIStyle();
+	//private string restartText;
+	//private string gameOverText;
+
+	private int score;
+	public Text scoreText;
+	public Text gameOverText;
+	public GameObject restartButton;
+
+	private bool gameOver;
+	//private bool restart;
+
+	void Start (){
+		/*
+		guiStyle.fontSize = 34;
+		guiStyle.alignment = TextAnchor.MiddleCenter;
+		guiStyle.normal.textColor = Color.white;
+		restartText = "";
+		gameOverText = "";
+		restart = false;
+		*/
+		gameOver = false;
+		gameOverText.text = "";
+		restartButton.SetActive (false);
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	/*
+	void Update (){
+		if (restart){
+			if (Input.GetKeyDown (KeyCode.R)){
+				SceneManager.LoadScene("Main");
+			}
+		}
+	}
+	*/
+
+	IEnumerator SpawnWaves (){
+		yield return new WaitForSeconds (startWait);
+		while (true){
+			for (int i = 0; i < hazardCount; i++){
+				GameObject hazard = hazards [Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);	
+
+			if (gameOver) {
+				//restartText = "Tekan 'R' untuk mulai! ";
+				//restart = true;
+				restartButton.SetActive (true);
+				break;
+			}
+		}
+	}
+		
+	/* versi lama
+	public void AddScore (int newScoreValue){
+		score += newScoreValue;
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(10, 10, 100, 20), "Nilai: " + score, guiStyle);
+		GUI.Label(new Rect(Screen.width /2, Screen.height /2, 100, 20), gameOverText, guiStyle);
+		GUI.Label(new Rect(Screen.width *2/3, 10, 100, 20), restartText, guiStyle);
+	}
+
+	public void GameOver(){
+		gameOverText = "Game Selesai! ";
+		gameOver = true;
+	}
+	*/
+
+	public void AddScore (int newScoreValue){
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore (){
+		scoreText.text = "Nilai: " + score;
+	}
+
+	public void GameOver (){
+		gameOverText.text = "Game Selesai! \n Terima kasih";
+		gameOver = true;
+	}
+
+	public void RestartGame () {
+		SceneManager.LoadScene("Main");
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Batas {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	public float kecepatan;
+	public float rotasi;
+	public Batas boundary;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
+	private Quaternion calibrationQuaternion;
+
+	void Start () {
+		KalibrasiAselerometer ();
+	}
+	
+	void Update (){
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+			GetComponent<AudioSource>().Play ();
+		}
+	}
+
+	//kalibrasi masukkan data dari sensor aselerometer
+	void KalibrasiAselerometer () {
+		Vector3 accelerationSnapshot = Input.acceleration;
+		Quaternion rotateQuaternion = Quaternion.FromToRotation (new Vector3 (0.0f, 0.0f, -1.0f), accelerationSnapshot);
+		calibrationQuaternion = Quaternion.Inverse (rotateQuaternion);
+	}
+
+	//dapatkan data yang sudah terkalibrasi
+	Vector3 FixAcceleration (Vector3 acceleration) {
+		Vector3 fixedAcceleration = calibrationQuaternion * acceleration;
+		return fixedAcceleration;
+	}
+
+	void FixedUpdate(){
+		//tahap1: komen kode sebelumnya
+		//float gerakHorisontal = Input.GetAxis ("Horizontal");
+		//float gerakVertikal = Input.GetAxis ("Vertical");
+		//Vector3 gerakan = new Vector3(gerakHorisontal, 0.0f, gerakVertikal);
+
+		//tahap2: tambahkan data dari sensor
+		Vector3 dataSensor = Input.acceleration;
+		Vector3 aselerasi = FixAcceleration (dataSensor);
+		Vector3 gerakan = new Vector3 (aselerasi.x, 0.0f, aselerasi.y);
+
+		GetComponent<Rigidbody>().velocity = gerakan * kecepatan;
+		GetComponent<Rigidbody>().position = new Vector3(
+			Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+		);
+		
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, 
+		                                                       GetComponent<Rigidbody>().velocity.x * -rotasi);
+	}
+}
